8e55086ba6f60cf25b310c5beb277891d67ea52a
Kiize <82241670+Kiize@users.noreply.github.com>
2023-05-11 17:11:45 +0200

    Update BoltzmannGPU.py

--- BoltzmannGPU.py
+++ BoltzmannGPU.py
@@ -10,59 +10,54 @@ import time
 start = time.time()
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-#Initial conditions
-velocity = 2000
-
+#Parameters
+velocity = 100
 n_particles = 5000
-r = torch.rand((2, n_particles)).to(device)
-ixr = r[0]>0.5
-ixl = r[0]<= 0.5
+radius = 5E-3
+gravity = -1E6
 
-ids = torch.arange(n_particles)
-
-bins = np.linspace(0, 3500, 60)
-vv = np.linspace(0, 3500, 1000)
+#Boltz Distribution
+bins = np.linspace(0, 3*velocity, 60)
+vv = np.linspace(0, 3*velocity, 1000)
 a = 6/velocity**2
 fv = a*vv*np.exp(-a*vv**2/2)
 
+#Initial conditions
+r = torch.rand((2, n_particles)).to(device)
+ixr = r[0]>0.5
+ixl = r[0]<= 0.5
+ids = torch.arange(n_particles)
 
 vsfer = torch.rand((2, n_particles)).to(device)
 vsfer[0] = vsfer[0] * velocity
 vsfer[1] = vsfer[1] * 2 * np.pi
 
+'''
+Uniform velocity modulo
+'''
+
 v = torch.zeros((2, n_particles)).to(device)
 v[0] = vsfer[0]*torch.cos(vsfer[1])
 v[1] = vsfer[0]*torch.sin(vsfer[1])
 
+g = torch.zeros((2,n_particles)).to(device)
+g[1] = gravity
 
 #Distances
-
 ids_pairs = torch.combinations(ids, 2).to(device)
-x_pairs = torch.combinations(r[0], 2).to(device)
-y_pairs = torch.combinations(r[1], 2).to(device)
+x_pairs = torch.stack([r[0][ids_pairs[:,0]], r[0][ids_pairs[:,1]]]).T
+y_pairs = torch.stack([r[1][ids_pairs[:,0]], r[1][ids_pairs[:,1]]]).T
 
 dx_pairs = torch.diff(x_pairs, axis = 1).ravel()
 dy_pairs = torch.diff(y_pairs, axis = 1).ravel()
 
 d_pairs = torch.sqrt(dx_pairs**2 + dy_pairs**2)
 
-#Velocities of collisions
-radius = 5E-3
-ids_pairs_collide = ids_pairs[d_pairs < 2*radius]
-
-v1 = v[:, ids_pairs_collide[:,0]]
-v2 = v[:, ids_pairs_collide[:,1]]
-r1 = r[:, ids_pairs_collide[:,0]]
-r2 = r[:, ids_pairs_collide[:,1]]
-
-v1new = v1 - torch.sum( (v1- v2)*(r1 -r2), axis = 0)/torch.sum((r1 - r2)**2, axis=0 ) * (r1 -r2)
-v2new = v2 - torch.sum( (v1- v2)*(r1 -r2), axis = 0)/torch.sum((r1 - r2)**2, axis=0 ) * (r2 -r1)
-
 #Functions
-def get_deltad_pairs(r):
-    dx = torch.diff(torch.combinations(r[0], 2).to(device)).squeeze()
-    dy = torch.diff(torch.combinations(r[1], 2).to(device)).squeeze()
-    return torch.sqrt(dx**2 + dy**2)
+def get_deltad_pairs(r, ids_pairs):
+    dx = torch.diff(torch.stack([r[0][ids_pairs[:,0]], r[0][ids_pairs[:,1]]]).T).squeeze()
+    dy = torch.diff(torch.stack([r[1][ids_pairs[:,0]], r[1][ids_pairs[:,1]]]).T).squeeze()
+    return dx**2 + dy**2
 
 def compute_new_v(v1, v2, r1, r2):
     v1new = v1 - torch.sum( (v1- v2)*(r1 -r2), axis = 0)/torch.sum((r1 - r2)**2, axis=0 ) * (r1 -r2)
@@ -72,12 +67,12 @@ def compute_new_v(v1, v2, r1, r2):
 def motion(r, v, id_pairs, ts, dt, d_cutoff):
     rs = torch.zeros((ts, r.shape[0], r.shape[1])).to(device)
     vs = torch.zeros((ts, v.shape[0], v.shape[1])).to(device)
-    #Stato iniziale
+    #Initial state
     rs[0] = r
     vs[0] = v
 
     for i in range(1, ts):
-        ic = id_pairs[get_deltad_pairs(r) < d_cutoff]
+        ic = id_pairs[get_deltad_pairs(r, ids_pairs) < d_cutoff**2]
         v[:, ic[:, 0]], v[:, ic[:, 1]] = compute_new_v(v[:, ic[:, 0]], v[:, ic[:, 1]], r[:, ic[:, 0]], r[:, ic[:, 1]])
 
         v[0, r[0]>1] = -torch.abs(v[0, r[0]>1])
@@ -85,7 +80,9 @@ def motion(r, v, id_pairs, ts, dt, d_cutoff):
         v[1, r[1]>1] = -torch.abs(v[1, r[1]>1])
         v[1, r[1]<0] = torch.abs(v[1, r[1]<0])
 
-        r = r + v*dt
+        r = r + v*dt + 1/2*g*(dt)**2
+        v = v + g*dt
+        
         rs[i] = r
         vs[i] = v
     return rs, vs
@@ -103,7 +100,7 @@ red, = ax[0].plot([], [], 'o', color = 'red', markersize = markersize)
 blue, = ax[0].plot([], [], 'o', color = 'blue', markersize = markersize)
 _, _, bar_container = ax[1].hist(torch.sqrt(torch.sum(vs[0]**2, axis = 0)).cpu(), bins=bins, density=True)
 ax[1].plot(vv, fv)
-ax[1].set_ylim(top=0.003)
+ax[1].set_ylim(top=np.max(fv) + 0.001)
 
 def prepare_animation(bar_container):
 
